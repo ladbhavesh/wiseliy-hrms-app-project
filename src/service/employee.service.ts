727138c48b9fee6965b98080876c24abaa4b6aef
ladbhavesh <lad.bhavesh@gmail.com>
2024-06-30 10:34:17 +0530

    added YARP proxy URL

--- src/service/employee.service.ts
+++ src/service/employee.service.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class EmployeeService {
-  private apiUrl = 'http://onlinetestapi.gerasim.in/api/TeamSync';
+  private apiUrl = 'http://localhost:5020/api/TeamSync';
   constructor(private http: HttpClient) { }
 
   getAllEmployee(): Observable<any> {
